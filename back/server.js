616e59f5ddd6d92ee2e995ec008374269ec73b84
Cecilio José Avila Ramos <110960338+CeciliojoseAvila@users.noreply.github.com>
2022-10-07 13:44:16 -0500

    clase del 07/10/2022

--- back/server.js
+++ back/server.js
@@ -1,8 +1,12 @@
 const app=require("./app")
-
+const connectDatabase = require("./configuracion/database");
 //Setear el archivo de configuración ////setear el archivo de configuracion (``, ``); node back/server.js , npm i nodemon --save-dev (esto para instalar nuevas dependencias)
 const dotenv=require("dotenv");
+const { connect } = require("mongoose");
 dotenv.config({path: 'back/configuracion/configur.env'})
+
+//configurar base de datos
+connectDatabase();
 
 //LAMEMOS AL SERVER
 const server=app.listen(process.env.PORT, () => {
@@ -11,4 +15,5 @@ const server=app.listen(process.env.PORT, () => {
 //luego de ejecutar= npm i nodemon --save-dev "se debió crear unos paquetes" y dentro de package.json modificamos los valores de "scripts": {
  //   "test": "echo \"Error: no test specified\" && exit 1"}
 //DESPUES DE MODIFICAR Y CREAR LOS NUEVOS= "start": "node back/server.js", "dev":"SET NODE_ENV=DEVELOPMENT& nodemon back/server", "prod":"SET NODE_ENV=PRODUCTION& nodemon back/server"
-// EJECUTAMOS EL SGTE COMANDO "npm run dev" sin las comillas.
+// EJECUTAMOS EL SGTE COMANDO "npm run dev" sin las comillas. 
+// para arrancar el servidor = npm run dev
